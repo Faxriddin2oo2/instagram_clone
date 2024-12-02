7da3efbebab6dd13444b147f9b9a4b05c22b481d
Faxriddin2oo2 <teshaboyevfaxriddin1@gmail.com>
2024-12-02 19:15:22 +0500

    Update settings.py

--- instagram_clone/settings.py
+++ instagram_clone/settings.py
@@ -128,7 +128,7 @@ WSGI_APPLICATION = 'instagram_clone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': config("DB_NAME"),
         "USER" : config('USER'),
         "PASSWORD": config("PASSWORD"),
