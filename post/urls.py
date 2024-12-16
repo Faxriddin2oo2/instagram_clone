7869c9e75596bf41c83c6f8090edaebd1274b93f
Faxriddin2oo2 <teshaboyevfaxriddin1@gmail.com>
2024-12-16 21:35:57 +0500

    Update

--- post/urls.py
+++ post/urls.py
@@ -4,5 +4,5 @@ from .views import PostListApiView, PostCreateView, PostRetrieveUpdateDestroyVie
 urlpatterns = [
     path('posts/', PostListApiView.as_view()),
     path('posts/create/', PostCreateView.as_view()),
-    path('posts/<uuid:pk>', PostRetrieveUpdateDestroyView.as_view()),
+    path('posts/<uuid:pk>/', PostRetrieveUpdateDestroyView.as_view()),
 ]
