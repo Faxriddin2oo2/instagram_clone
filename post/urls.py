ceff5edb60aad86eea9608bcd1db5898555fe353
Faxriddin2oo2 <teshaboyevfaxriddin1@gmail.com>
2024-12-17 23:55:58 +0500

    Post va izohlar layklari roʻyxatini chiqarish '

--- post/urls.py
+++ post/urls.py
@@ -1,13 +1,24 @@
 from django.urls import path
 from .views import PostListApiView, PostCreateView, PostRetrieveUpdateDestroyView, PostCommentListView, \
-    PostCommentCreateView, CommentListCreateApiView
+    PostCommentCreateView, CommentListCreateApiView, PostLikeListView, CommentRetrieveView, CommentLikeListView
 
 urlpatterns = [
     path('list/', PostListApiView.as_view()),
     path('create/', PostCreateView.as_view()),
     path('<uuid:pk>/', PostRetrieveUpdateDestroyView.as_view()),
+    path('<uuid:pk>/likes/', PostLikeListView.as_view()),
     path('<uuid:pk>/comments/', PostCommentListView.as_view()),
     path('<uuid:pk>/comments/create/', PostCommentCreateView.as_view()),
 
     path('comments/', CommentListCreateApiView.as_view()),
+    path('comments/<uuid:pk>/', CommentRetrieveView.as_view()),
+    path('comments/<uuid:pk>/likes/', CommentLikeListView.as_view()),
+
+
+    # Vazifa
+    """
+    path('likes/')
+    path('likes/create/')
+    path(likes/delete/'
+    """
 ]
